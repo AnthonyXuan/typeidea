5786398c3ebe4d6928b7e7774e1c955fcb20aff3
Anthony Xuan <anthony2xuan@gmail.com>
2022-08-18 23:26:53 +0800

    update

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,81 @@
+from django.urls import reverse
+from django.utils.html import format_html
 from django.contrib import admin
-
+from .models import Post, Tag, Category
 # Register your models here.
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前的用户的分类 """
+    
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+    
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+    
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(id=category_id)
+        return queryset
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','status','is_nav', 'post_count', 'created_time')
+    fields = ('name', 'status', 'is_nav')
+    
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
+    
+    def post_count(self, obj):
+        return obj.post_set.count()
+    
+    post_count.short_description = '文章数量'
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','status','created_time')
+    fields = ('name', 'status')
+    
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','category','status','created_time', 'operator')
+    list_display_links = []
+    
+    list_filter = [CategoryOwnerFilter]
+    # ? category__name里的双下划线: 一种特殊用法，指定搜索了关联Model的数据
+    search_fields = ['title', 'category__name']
+    
+    actions_on_top = True
+    actions_on_bottom = True
+    
+    save_on_top = True
+    
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+    
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    
+    operator.short_description = '操作'
+    
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
+    
+    def get_queryset(self, request):
+        qs = admin.ModelAdmin.get_queryset(request) 
+        return qs.filter(owner=request.user)
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -7,6 +7,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_ITEMS = (
@@ -24,6 +28,10 @@ class Category(models.Model):
         verbose_name = verbose_name_plural = "分类"
 
 class Tag(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_ITEMS = (
@@ -40,6 +48,10 @@ class Tag(models.Model):
         verbose_name = verbose_name_plural = "标签"
         
 class Post(models.Model):
+    
+    def __str__(self):
+        return self.title
+    
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_DRAFE = 2
@@ -51,9 +63,10 @@ class Post(models.Model):
     
     title = models.CharField(max_length=225, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
-    content = models.TextField(verbose_name="格式", help_text="正文必须为markdown格式")
+    content = models.TextField(verbose_name="内容", help_text="正文必须为markdown格式")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     
